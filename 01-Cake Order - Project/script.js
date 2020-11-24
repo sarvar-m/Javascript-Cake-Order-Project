97dbc02569941ecd7bdd27aa747762bdeda80879
Sarvar <73389120+sarvar-m@users.noreply.github.com>
2020-11-25 01:20:20 +0700

    Updated script.js

--- 01-Cake Order - Project/script.js
+++ 01-Cake Order - Project/script.js
@@ -27,59 +27,6 @@ const cakeType = document.getElementById('cakeSelect');
 const orderAmount = document.getElementById('cakeAmount');
 const orderBtn = document.getElementById('submit_btn');
 
-/*
-const checkOrder = (order) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-
-      let inStock = patisserie[order[0]].stock >= order[1];
-
-      if(inStock) {
-        let totalPayment = 0;
-        totalPayment = order[1] * patisserie[order[0]].price;
-        console.log(`You ordered ${order[1]} ${order[0]}`)
-        console.log(`All of the items are in stock. The total cost of the order is ${totalPayment}.`);
-        resolve([order, totalPayment]);
-      } else {
-        reject(`The order could not be completed because some items are sold out.`);
-      }
-            
-    }, 1000)
-  })
-}
-
-const payment = (resolvedValueArray) => {
-  const order = resolvedValueArray[0];
-  const totalPayment = resolvedValueArray[1];
-  return new Promise((resolve, reject) => {
-    //setTimeout
-    setTimeout(() => {
-      if(confirm('Do you confirm this order?')) {
-        console.log(`Payment completed. You paid $${totalPayment}.`);
-        resolve(order);
-      }
-    }, 2000)
-  })  
-}
-
-const stockControl = (resolvedValueArray) => {
-  return new Promise((resolve, reject) => {
-    //setTimeout
-    setTimeout(() => {
-      patisserie[order[0]].stock = patisserie[order[0]].stock - order[1];
-      if(patisserie[order[0]].stock < 5) {
-        resolve(`To Cashier: Please wait for checking stock...${order[0]} stock is ${patisserie[order[0]].stock} and it is critical`)
-      }else{
-        resolve(`We have enough stock for this item. No need to worry.`);
-      }
-    }, 3000)
-  })  
-    //setTimeout
-
-  }
-*/
-
-
 
 orderBtn.onclick = ()=>{
 
